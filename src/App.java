0b906dfcc3148e2224d40dcf1807a4281dd36c75
zuki <zukidono@gmail.com>
2024-07-09 00:00:22 +0700

    added dao

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import dao.Student;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Student student = new Student("zuki", 23);
+        System.out.printf("Hello, World!, %s\n", student.name);
     }
 }
---
+++ src/dao/Student.java
@@ -0,0 +1,11 @@
+package dao;
+
+public class Student {
+    public String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
